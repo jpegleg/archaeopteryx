3c38e137f335d4c2e17a932e9bf5f7f3ed94c83c
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-03-12 20:48:34 -0400

    Update main.rs
    expand scope to system data collection before generation via unix command output, also to STDOUT

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use std::str;
-
+use std::process::Command;
 use chrono::prelude::*;
-
 use openssl::rsa::Rsa;
 use openssl::pkey::PKey;
 
 fn main() {
+    let output = Command::new("sh")
+      .arg("-c")
+      .arg("hostname; whoami; hostname -I; b2sum /boot/* 2>/dev/null; b2sum $(which archaeopteryx); ps auxww; w;")
+      .output()
+      .expect("Failed to execute command");
     let readi: DateTime<Utc> = Utc::now();
-    println!("{}", readi);
+    println!("{} {:?}", readi, output);
     let rsa = Rsa::generate(4096).unwrap();
     let pkey = PKey::from_rsa(rsa).unwrap();
     let pub_key: Vec<u8> = pkey.public_key_to_pem().unwrap();
     let pri_key: Vec<u8> = pkey.private_key_to_pem_pkcs8().unwrap();
-
     println!("{}", str::from_utf8(pub_key.as_slice()).unwrap());
     println!("{}", str::from_utf8(pri_key.as_slice()).unwrap());
-
 }
